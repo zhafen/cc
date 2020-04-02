5d490d59d5e8c21da4b0454232f1a20b3603e595
Zachary Hafen <zhafen@u.northwestern.edu>
2020-04-02 16:42:23 -0500

    Added importing of ADS data.

--- cc/publication.py
+++ cc/publication.py
@@ -1,3 +1,6 @@
+import ads
+import bibtexparser
+
 import augment
 
 ########################################################################
@@ -11,16 +14,74 @@ class Publication( object ):
 
     ########################################################################
 
-    def load_full_tex( self, filepath ):
+    def get_ads_data( self, arxiv_number ):
+        '''Retrieve all data the NASA Astrophysical Data System has regarding
+        a paper.
+
+        NOTE: For this to work you MUST have your ADS API key
+        saved to ~/.ads/dev_key
+
+        Args:
+            arxiv_number (str):
+                Arxiv number to search ads for.
+
+        Returns:
+            self.ads_data (ads.search.Article):
+                Class containing ADS data.
+        '''
+
+        self.ads_query = ads.SearchQuery(arxiv=arxiv_number)
+        self.ads_data = list( self.ads_query )[0]
+
+        return self.ads_data
+
+    ########################################################################
+
+    def read_citation( self, bibtex_fp, ):
+        '''Retrieve a citation from a BibTex file.
+
+        Args:
+            bibtex_fp (str):
+                Filepath containing the BibTex file to read from.
+
+        Returns:
+            self.citation (dict):
+                A dictionary containing entries for a BibTex-style citation.
+        '''
+
+        # Load the database
+        with open( bibtex_fp ) as bibtex_file:
+            bib_database = bibtexparser.load(bibtex_file)
+
+        # Find the relevant citation
+        matches = []
+        for citation in bib_database.entries:
+            if citation['ID'] == self.citation_key:
+                matches.append( citation )
+
+        # Parse results of search
+        if len( matches ) < 1:
+            raise Exception( 'Citation not found in file.' )
+        elif len( matches ) > 1:
+            raise Exception( 'Duplicate entries found in file.' )
+
+        # Store result
+        self.citation = matches[0]
+
+        return self.citation
+
+    ########################################################################
+
+    def load_full_tex( self, tex_fp ):
         '''Loads a tex file for further manipulation.
 
         Args:
-            filepath (str):
+            tex_fp (str):
                 Location of tex file to load.
         '''
 
         # Retrieve full text
         self.full_text = []
-        with open( filepath ) as f:
+        with open( tex_fp ) as f:
             for line in f:
                 self.full_text.append( line )  
---
+++ cc/tests/test_publication.py
@@ -0,0 +1,48 @@
+from mock import patch
+import numpy as np
+import numpy.testing as npt
+import os
+import unittest
+
+import cc.publication
+
+########################################################################
+
+filepath = './tests/data/arxiv_source/Hafen2019/CGM_origin.tex'
+
+class TestRetrieveMetadata( unittest.TestCase ):
+
+    def test_get_ads_data( self ):
+
+        # Load
+        p = cc.publication.Publication( 'Hafen2019' )
+
+        p.get_ads_data( '1811.11753' )
+
+        assert p.ads_data.title == ['The origins of the circumgalactic medium in the FIRE simulations']
+
+    ########################################################################
+
+    def test_read_citation( self ):
+
+        # Load
+        p = cc.publication.Publication( 'Hafen2019' )
+
+        bibtex_fp = './tests/data/example.bib'
+
+        p.read_citation( bibtex_fp )
+
+        assert p.citation['eprint'] == '1811.11753'
+
+########################################################################
+
+class TestTex( unittest.TestCase ):
+
+    def test_load_full_tex( self ):
+
+        # Load
+        p = cc.publication.Publication( 'Hafen2019' )
+
+        p.load_full_tex( filepath )
+
+        assert p.full_text[0] == '% mnras_template.tex \n'
